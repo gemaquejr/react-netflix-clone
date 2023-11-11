3e7410589a6e6598a4b0039337c1ca0837472ecc
gemaquejr <gemaquejr@hotmail.com>
2023-11-11 10:57:12 -0300

    Adiciona componente FeaturedMovie completo
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from "react";
 import Tmdb from './services/Tmdb';
 import MovieRow from "./components/MovieRow/MovieRow";
 import './App.css'
+import FeaturedMovie from "./components/FeaturedMovie/FeaturedMovie";
 
 const App = () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
   
     loadAll();
@@ -18,6 +26,12 @@ const App = () => {
 
   return(
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from './FeaturedMovie.module.css'
+
+export default function FeaturedMovie({item}) {
+    
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name );
+    }
+
+
+    return (
+        <section className={styles.featured} style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className={styles.featured_vertical}>
+                <div className={styles.featured_horizontal}>
+                    <div className={styles.featured_name}>{item.original_name}</div>
+                    <div className={styles.featured_info}>
+                        <div className={styles.featured_points}>{item.vote_average} pontos</div>
+                        <div className={styles.featured_year}>{firstDate.getFullYear()}</div>
+                        <div className={styles.featured_seasons}>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className={styles.featured_description}>{item.overview}</div>
+                    <div className={styles.featured_buttons}>
+                        <a href={`/watch/${item.id}`} className={styles.featured_playbutton}> ▶ Assistir</a>
+                        <a href={`/list/add/${item.id}`} className={styles.featured_mylistbutton}>+ Minha Lista</a>
+
+                    </div>
+                    <div className={styles.featured_genres}><strong>Gêneros: </strong>{genres.join(', ')}</div>
+                </div>
+            </div>
+        </section>
+    )
+}
