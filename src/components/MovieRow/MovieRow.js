e9301e3bd6d568a711f9b960e4c8bd3ebcaf1b03
gemaquejr <gemaquejr@hotmail.com>
2023-11-11 14:28:38 -0300

    Adiciona lógica dos botões de navegação em MovieRow
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './MovieRow.module.css'
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default function MovieRow({title, items}) {
+    const [scrollX, setScrollX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listWidth = items.results.length * 150;
+        if((window.innerWidth - listWidth) > x) {
+            x = (window.innerWidth - listWidth) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className={styles.movieRow}>
             <h2>{title}</h2>
-            <div className={styles.movieRow_left}>
+            <div className={styles.movieRow_left} onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{fontSize: 50}} />
             </div>
-            <div className={styles.movieRow_right}>
+            <div className={styles.movieRow_right} onClick={handleRightArrow}>
                 <NavigateNextIcon style={{fontSize: 50}} />
             </div>
             <div className={styles.movieRow_listarea}>
-                <div className={styles.movieRow_list}>
+                <div className={styles.movieRow_list} style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className={styles.movieRow_item}>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
