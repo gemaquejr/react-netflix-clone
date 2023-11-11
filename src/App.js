116e71fc19f7ecd5c605a818197983b2eff58a8c
gemaquejr <gemaquejr@hotmail.com>
2023-11-10 23:55:11 -0300

    Adiciona componente MovieRow
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from './services/Tmdb';
+import MovieRow from "./components/MovieRow/MovieRow";
 
 const App = () => {
 
@@ -17,10 +18,8 @@ const App = () => {
   return(
     <div className="page">
       <section className="lists">
-        {movieList.map((item) => (
-          <li key={ item.id }>
-            {item.title}
-          </li>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div >
