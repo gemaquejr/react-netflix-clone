8061d9aa2eea2689ef09871c349adf522756dbd7
gemaquejr <gemaquejr@hotmail.com>
2023-11-11 14:04:35 -0300

    Adiciona botões de navegação em MovieRow
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from './MovieRow.module.css'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default function MovieRow({title, items}) {
     return (
         <div className={styles.movieRow}>
             <h2>{title}</h2>
+            <div className={styles.movieRow_left}>
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+            </div>
+            <div className={styles.movieRow_right}>
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
             <div className={styles.movieRow_listarea}>
                 <div className={styles.movieRow_list}>
                     {items.results.length > 0 && items.results.map((item, key) => (
