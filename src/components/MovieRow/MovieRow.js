612f968d7c48c7f74603c62ef8e05bcba695d174
gemaquejr <gemaquejr@hotmail.com>
2023-11-11 00:56:27 -0300

    Adiciona module.css de MovieRow
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -3,12 +3,16 @@ import styles from './MovieRow.module.css'
 
 export default function MovieRow({title, items}) {
     return (
-        <div>
+        <div className={styles.movieRow}>
             <h2>{title}</h2>
-            <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-                ))}
+            <div className={styles.movieRow_listarea}>
+                <div className={styles.movieRow_list}>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className={styles.movieRow_item}>
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}
+                </div>       
             </div>
         </div>
     )
