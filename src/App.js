87cf50de8fa980ac1f72a750ee6e8d939206bc3a
gemaquejr <gemaquejr@hotmail.com>
2023-11-10 23:09:39 -0300

    Adiciona estado para salvar a lista de filmes em APP.js
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Tmdb from './services/Tmdb';
 
 const App = () => {
 
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
   
     loadAll();
   }, [])
 
   return(
-    <div>
-      Olá mundo!
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item) => (
+          <li key={ item.id }>
+            {item.title}
+          </li>
+        ))}
+      </section>
     </div >
   );
 }
